c132e5cfbf52c44803b5d598b9f2d114cc23a3b7
Wanderson Aldo <wanderson.aldo@gmail.com>
2020-02-22 09:59:23 -0300

    Signature capture

---
+++ src/components/myComp.js
@@ -0,0 +1,115 @@
+/*Example to Capture Signature in React Native*/
+
+import React, { Component } from 'react';
+
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Text,
+  StatusBar,
+  TouchableHighlight,
+} from 'react-native';
+ 
+import SignatureCapture from 'react-native-signature-capture';
+
+
+class App extends Component {
+
+  saveSign = () => {
+    this.refs['sign'].saveImage();
+  };
+ 
+  resetSign = () => {
+    this.refs['sign'].resetImage();
+  };
+ 
+  _onSaveEvent = result => {
+    //result.encoded - for the base64 encoded png
+    //result.pathName - for the file path name
+    alert('Signature Captured Successfully');
+    console.log(result.encoded);
+  };
+ 
+  _onDragEvent = () => {
+    // This callback will be called when the user enters signature
+    console.log('dragged');
+  };
+ 
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView style={{ flex: 1 }}>
+          <View style={styles.body}>
+            <View style={{ flex: 1, flexDirection: 'column' }}>
+              <Text style={styles.header}>
+                Example to Capture Signature in React Native
+              </Text>
+              <SignatureCapture
+                style={styles.signature}
+                ref="sign"
+                onSaveEvent={this._onSaveEvent}
+                onDragEvent={this._onDragEvent}
+                showNativeButtons={false}
+                showTitleLabel={false}
+                viewMode={'portrait'}
+              />
+              <View style={{ flexDirection: 'row' }}>
+                <TouchableHighlight
+                  style={styles.buttonStyle}
+                  onPress={() => {
+                    this.saveSign();
+                  }}>
+                  <Text>Save</Text>
+                </TouchableHighlight>
+                <TouchableHighlight
+                  style={styles.buttonStyle}
+                  onPress={() => {
+                    this.resetSign();
+                  }}>
+                  <Text>Reset</Text>
+                </TouchableHighlight>
+              </View>
+            </View>
+          </View>
+        </SafeAreaView>
+      </View>
+    );
+  }
+}
+ 
+
+const styles = StyleSheet.create({
+
+  body: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+
+  header: {
+    fontSize: 22,
+    fontWeight: '600',
+    color: 'black',
+    textAlign: 'center',
+    paddingVertical: 20,
+  },
+
+  signature: {
+    flex: 1,
+    borderColor: '#000033',
+    borderWidth: 1,
+  },
+
+  buttonStyle: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 50,
+    backgroundColor: '#eeeeee',
+    margin: 10,
+  },
+  
+});
+ 
+export default App;
